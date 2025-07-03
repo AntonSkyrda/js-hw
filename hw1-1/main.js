97fbb6f942fde8df5bc4731b0aaafeb9050a0bd6
AntonSkyrda <mr.skyrda@gmail.com>
2025-07-03 06:01:49 +0300

    done hw1 1st task

---
+++ hw1-1/main.js
@@ -0,0 +1,28 @@
+let hello = "hello";
+let okten = "okten";
+let com_ = "com";
+let ua = "ua"
+let one = 1;
+let ten = 10;
+let tripleNine = -999;
+let oneTwoThree = 123;
+const PI = 3.14;
+let floatNumber = 2.7
+let sixteen = 16;
+let boolTrue = true;
+let boolFalse = false;
+
+console.log(hello);
+console.log(okten);
+console.log(com_);
+console.log(ua);
+console.log(one);
+console.log(ten);
+console.log(tripleNine);
+console.log(oneTwoThree);
+console.log(PI);
+console.log(floatNumber);
+console.log(sixteen);
+console.log(boolTrue);
+console.log(boolFalse);
+
