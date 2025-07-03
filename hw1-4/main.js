79a65e2efdc9e806021ff7ba03b86c374ec9c117
AntonSkyrda <mr.skyrda@gmail.com>
2025-07-03 06:11:41 +0300

    done hw1 4th task

---
+++ hw1-4/main.js
@@ -0,0 +1,7 @@
+let firstName = prompt("Enter your first name?");
+let middleName = prompt("Enter your middle name?");
+let lastName = prompt("Enter your last name?");
+
+let person = `${firstName} ${middleName} ${lastName}`;
+
+console.log(person);
