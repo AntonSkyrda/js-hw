da5131033adb500b393f0c5ca2589c95e47c150d
AntonSkyrda <mr.skyrda@gmail.com>
2025-07-07 03:55:39 +0300

    done hw4 7th task

---
+++ hw4/hw4-7/main.js
@@ -0,0 +1,12 @@
+function addUl(text, repeatCount) {
+    document.write("<ul>")
+    for (let i = 0; i <repeatCount; i++) {
+        document.write(
+            `<li>${text}</li>
+            `
+        )
+    }
+    document.write("</ul>")
+}
+
+addUl("qwe", 10)
